20e62f18d63312e6d182f68781f2c8c54bc8252a
NeMe313 <zvezdankiza@yahoo.com>
2021-02-03 16:13:16 +0100

    Robot Builder Design Pattern

---
+++ RobotBuilder.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+public interface RobotBuilder {
+	
+	public void buildRobotHead();
+	public void buildRobotTorso();
+	public void buildRobotArms();
+	public void buildRobotLegs();
+	
+	public Robot getRobot();
+	
+}
